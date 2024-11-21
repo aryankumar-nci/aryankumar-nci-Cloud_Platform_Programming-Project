c900765bc950fefd363305ea2d8ce3a766a944a9
Aryan Kumar <x23310618@student.ncirl.ie>
2024-11-21 21:28:50 +0000

    CPP commit-1

---
+++ main/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Listing
+
+class ListingAdmin(admin.ModelAdmin):
+    readonly_fields=('id',)
+    
+admin.site.register(Listing,ListingAdmin)
+
+
+
+
+
